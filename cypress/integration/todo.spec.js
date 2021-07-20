49f8d1a9c638394fca82f8a21c5ed129ad8a09a1
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-07-20 14:52:59 +0300

    Task 1

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -9,13 +9,13 @@ describe('Register and log in to-do app', () => {
   it('Test Case', () => {
     cy.log("Hii!")
   })
-/*
-  it('Register', () => {
+
+  it.skip('Register with correct data', () => {
     cy.get('#customer_menu_top').click();
     cy.get('#accountFrm > fieldset > button').click();
     cy.get('#AccountFrm_firstname').type('New');
     cy.get('#AccountFrm_lastname').type('User');
-    cy.get('#AccountFrm_email').type('email.valid@gmail.com');
+    cy.get('#AccountFrm_email').type('email.valid.nou@gmail.com');
     cy.get('#AccountFrm_telephone').type('0756892576');
     cy.get('#AccountFrm_fax').type('0756892576');
     cy.get('#AccountFrm_company').type('My company srl');
@@ -25,30 +25,31 @@ describe('Register and log in to-do app', () => {
     cy.get('#AccountFrm_country_id').select('Romania').should('have.value','175');
     cy.get('#AccountFrm_zone_id').select('Alba').should('have.value','2679');
     cy.get('#AccountFrm_postcode').type('5666');
-    cy.get('#AccountFrm_loginname').type('contulmeuu');
+    cy.get('#AccountFrm_loginname').type('contulmeu2');
     cy.get('#AccountFrm_password').type('parola123');
     cy.get('#AccountFrm_confirm').type('parola123');
     cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
     cy.get('#AccountFrm_agree').click();
     cy.get('#AccountFrm > div.form-group > div > div > button').click();
     cy.get('#maincontainer').click();
+    cy.log('Successful register');
   })
-*/
-/*
-  it('Login', () => {
+
+  it.skip('Login with valid account', () => {
       cy.get('#customer_menu_top').click();
       cy.get('#loginFrm_loginname').type('contulmeunou');
       cy.get('#loginFrm_password').type('parola123');
       cy.get('#loginFrm > fieldset > button').click();
+      cy.log('Successful login');
   })
-*/
 
-it('Register', () => {
+
+it.skip('Register with invalid email', () => {
   cy.get('#customer_menu_top').click();
   cy.get('#accountFrm > fieldset > button').click();
   cy.get('#AccountFrm_firstname').type('123');
   cy.get('#AccountFrm_lastname').type('User');
-  cy.get('#AccountFrm_email').type('email.validdd@gmail.com');
+  cy.get('#AccountFrm_email').type('email.invalid');
   cy.get('#AccountFrm_telephone').type('0756892576');
   cy.get('#AccountFrm_fax').type('0756892576');
   cy.get('#AccountFrm_company').type('My company srl');
@@ -58,13 +59,194 @@ it('Register', () => {
   cy.get('#AccountFrm_country_id').select('Romania').should('have.value','175');
   cy.get('#AccountFrm_zone_id').select('Alba').should('have.value','2679');
   cy.get('#AccountFrm_postcode').type('5666');
-  cy.get('#AccountFrm_loginname').type('contulmeuuuu');
+  cy.get('#AccountFrm_loginname').type('contulmeunouu');
+  cy.get('#AccountFrm_password').type('parola123');
+  cy.get('#AccountFrm_confirm').type('parola123');
+  cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
+  cy.get('#AccountFrm_agree').click();
+  cy.get('#AccountFrm > div.form-group > div > div > button').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('Email Address does not appear to be valid!');
+  cy.log('Register failed');
+})
+
+//not validation: Phone number is incorrect 
+it('Register with invalid phone number', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#accountFrm > fieldset > button').click();
+  cy.get('#AccountFrm_firstname').type('123');
+  cy.get('#AccountFrm_lastname').type('User');
+  cy.get('#AccountFrm_email').type('email.nooou@gmail.com');
+  cy.get('#AccountFrm_telephone').type('07568925');
+  cy.get('#AccountFrm_fax').type('07568925');
+  cy.get('#AccountFrm_company').type('My company srl');
+  cy.get('#AccountFrm_address_1').type('Suceava');
+  cy.get('#AccountFrm_address_2').type('Bucuresti');
+  cy.get('#AccountFrm_city').type('Suceava');
+  cy.get('#AccountFrm_country_id').select('Romania').should('have.value','175');
+  cy.get('#AccountFrm_zone_id').select('Alba').should('have.value','2679');
+  cy.get('#AccountFrm_postcode').type('5666');
+  cy.get('#AccountFrm_loginname').type('conttulmeunouunout');
   cy.get('#AccountFrm_password').type('parola123');
   cy.get('#AccountFrm_confirm').type('parola123');
   cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
   cy.get('#AccountFrm_agree').click();
   cy.get('#AccountFrm > div.form-group > div > div > button').click();
   cy.get('#maincontainer').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible');//expected
+ 
+}) 
+
+
+it.skip('Register with invalid name', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#accountFrm > fieldset > button').click();
+  cy.get('#AccountFrm_lastname').type('User');
+  cy.get('#AccountFrm_email').type('email');
+  cy.get('#AccountFrm_telephone').type('0756892585');
+  cy.get('#AccountFrm_fax').type('0756892585');
+  cy.get('#AccountFrm_company').type('My company srl');
+  cy.get('#AccountFrm_address_1').type('Suceava');
+  cy.get('#AccountFrm_address_2').type('Bucuresti');
+  cy.get('#AccountFrm_city').type('Suceava');
+  cy.get('#AccountFrm_country_id').select('Romania').should('have.value','175');
+  cy.get('#AccountFrm_zone_id').select('Alba').should('have.value','2679');
+  cy.get('#AccountFrm_postcode').type('5666');
+  cy.get('#AccountFrm_loginname').type('noulmeucont');
+  cy.get('#AccountFrm_password').type('parola123');
+  cy.get('#AccountFrm_confirm').type('parola123');
+  cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
+  cy.get('#AccountFrm_agree').click();
+  cy.get('#AccountFrm > div.form-group > div > div > button').click();
+  //cy.get('#maincontainer').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('First Name must be between 1 and 32 characters!');
+  cy.get('.help-block').contains('First Name must be between 1 and 32 characters!');
+  cy.log('Register failed');
+}) 
+
+it.skip('Register with invalid name >32', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#accountFrm > fieldset > button').click();
+  cy.get('#AccountFrm_firstname').type('12312311111111111111111112222222222');
+  cy.get('#AccountFrm_lastname').type('User');
+  cy.get('#AccountFrm_email').type('email@gmail.com');
+  cy.get('#AccountFrm_telephone').type('0756892585');
+  cy.get('#AccountFrm_fax').type('0756892585');
+  cy.get('#AccountFrm_company').type('My company srl');
+  cy.get('#AccountFrm_address_1').type('Suceava');
+  cy.get('#AccountFrm_address_2').type('Bucuresti');
+  cy.get('#AccountFrm_city').type('Suceava');
+  cy.get('#AccountFrm_country_id').select('Romania').should('have.value','175');
+  cy.get('#AccountFrm_zone_id').select('Alba').should('have.value','2679');
+  cy.get('#AccountFrm_postcode').type('5666');
+  cy.get('#AccountFrm_loginname').type('noulmeucont');
+  cy.get('#AccountFrm_password').type('parola123');
+  cy.get('#AccountFrm_confirm').type('parola123');
+  cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
+  cy.get('#AccountFrm_agree').click();
+  cy.get('#AccountFrm > div.form-group > div > div > button').click();
+  //cy.get('#maincontainer').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('First Name must be between 1 and 32 characters!');
+  cy.get('.help-block').contains('First Name must be between 1 and 32 characters!');
+  cy.log('Register failed');
+}) 
+
+
+it.skip('Register with invalid login name', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#accountFrm > fieldset > button').click();
+  cy.get('#AccountFrm_firstname').type('Cont123');
+  cy.get('#AccountFrm_lastname').type('User');
+  cy.get('#AccountFrm_email').type('email@gmail.com');
+  cy.get('#AccountFrm_telephone').type('0756892585');
+  cy.get('#AccountFrm_fax').type('0756892585');
+  cy.get('#AccountFrm_company').type('My company srl');
+  cy.get('#AccountFrm_address_1').type('Suceava');
+  cy.get('#AccountFrm_address_2').type('Bucuresti');
+  cy.get('#AccountFrm_city').type('Suceava');
+  cy.get('#AccountFrm_country_id').select('Romania').should('have.value','175');
+  cy.get('#AccountFrm_zone_id').select('Alba').should('have.value','2679');
+  cy.get('#AccountFrm_postcode').type('5666');
+  cy.get('#AccountFrm_loginname').type('cont');
+  cy.get('#AccountFrm_password').type('parola123');
+  cy.get('#AccountFrm_confirm').type('parola123');
+  cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
+  cy.get('#AccountFrm_agree').click();
+  cy.get('#AccountFrm > div.form-group > div > div > button').click();
+  //cy.get('#maincontainer').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('Login name must be alphanumeric only and between 5 and 64 characters!');
+  cy.get('#AccountFrm > div:nth-child(9) > fieldset > div:nth-child(1) > span').contains('Login name must be alphanumeric only and between 5 and 64 characters!');
+  cy.log('Register failed');
+}) 
+
+
+//Error: Password confirmation does not match password!
+it.skip('Register with invalid password confirm', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#accountFrm > fieldset > button').click();
+  cy.get('#AccountFrm_firstname').type('Cont123');
+  cy.get('#AccountFrm_lastname').type('User');
+  cy.get('#AccountFrm_email').type('email1@gmail.com');
+  cy.get('#AccountFrm_telephone').type('0756892585');
+  cy.get('#AccountFrm_fax').type('0756892585');
+  cy.get('#AccountFrm_company').type('My company srl');
+  cy.get('#AccountFrm_address_1').type('Suceava');
+  cy.get('#AccountFrm_address_2').type('Bucuresti');
+  cy.get('#AccountFrm_city').type('Suceava');
+  cy.get('#AccountFrm_country_id').select('Romania').should('have.value','175');
+  cy.get('#AccountFrm_zone_id').select('Alba').should('have.value','2679');
+  cy.get('#AccountFrm_postcode').type('5666');
+  cy.get('#AccountFrm_loginname').type('cont');
+  cy.get('#AccountFrm_password').type('parola123');
+  cy.get('#AccountFrm_confirm').type('parola1234');
+  cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
+  cy.get('#AccountFrm_agree').click();
+  cy.get('#AccountFrm > div.form-group > div > div > button').click();
+ cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('Password confirmation does not match password!');
+ cy.get('#AccountFrm > div:nth-child(9) > fieldset > div.form-group.has-error > span').contains('Password confirmation does not match password!');
+ cy.log('Register failed');
+}) 
+
+it.skip('Register with empty fields', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#accountFrm > fieldset > button').click();
+  cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
+  cy.get('#AccountFrm_agree').click();
+  cy.get('#AccountFrm > div.form-group > div > div > button').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('Login name must be alphanumeric only and between 5 and 64 characters!')
+  .contains('First Name must be between 1 and 32 characters!')
+  .contains('Last Name must be between 1 and 32 characters!')
+  .contains('Email Address does not appear to be valid!')
+  .contains('Address 1 must be between 3 and 128 characters!')
+  .contains('City must be between 3 and 128 characters!')
+  .contains('Zip/postal code must be between 3 and 10 characters!')
+  .contains('Please select a region / state!')
+  .contains('Password must be between 4 and 20 characters!');
+  cy.log('Register failed');
+}) 
+
+it.skip('Login with invalid login name', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#loginFrm_loginname').type('cont');
+  cy.get('#loginFrm_password').type('parola123');
+  cy.get('#loginFrm > fieldset > button').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible').contains('Error: Incorrect login or password provided.');
+  cy.log('Filed login');
+})
+
+it.skip('Login with invalid password', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#loginFrm_loginname').type('contulmeunou');
+  cy.get('#loginFrm_password').type('parola123456');
+  cy.get('#loginFrm > fieldset > button').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible').contains('Error: Incorrect login or password provided.');
+  cy.log('Filed login');
+})
+
+it.skip('Login with empty fields', () => {
+  cy.get('#customer_menu_top').click();
+  cy.get('#loginFrm > fieldset > button').click();
+  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible').contains('Error: Incorrect login or password provided.');
+  cy.log('Filed login');
 })
 
 })
