5d86e260814070052a4926028403abb7a4ea03c9
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-07-22 23:25:33 +0300

    Task3

--- cypress/integration/task3.spec.js
+++ cypress/integration/task3.spec.js
@@ -44,27 +44,65 @@ it('wishlist', ()=>{
     cy.get('#customer_menu_top > li > a > div').click();
     cy.get('#maincontainer > div > div.column_right.col-md-3.col-xs-12.mt20 > div.sidewidt > div > ul > li:nth-child(2) > a').click();
     cy.get('#maincontainer > div > div.col-md-9.col-xs-12.mt20 > div > div > div > table > tbody > tr.wishlist_52 > td:nth-child(6) > a.btn.btn-sm.btn-default.btn-remove > i').click({force: true})
-    cy.get('#maincontainer > div > div.col-md-9.col-xs-12.mt20 > div > div > div > div > a.btn.btn-default.mr10').click();
+    //cy.get('#maincontainer > div > div.col-md-9.col-xs-12.mt20 > div > div > div > div > a.btn.btn-default.mr10').click();
+    
 })
 
-/*
+let orderId
+let orderId2
 it('purchasing items', ()=>{
-    
+    let address1
+    cy.get('#maincontainer > div > div.column_right.col-md-3.col-xs-12.mt20 > div.sidewidt > div > ul > li:nth-child(5) > a').click()
+    cy.get("#maincontainer > div > div.col-md-9.col-xs-12.mt20 > div > div.contentpanel > div.genericbox.border-bottom > table > tbody > tr > td:nth-child(1) > address").invoke("text").then((text)=>{
+        address1=text.substring(text.indexOf("Popescu")+"Popescu".length, text.indexOf("Bucuresti "));
+        cy.log(address1)
+})
     cy.get('#categorymenu > nav > ul > li:nth-child(2) > a').click();
     cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div:nth-child(6) > div.thumbnail > div.pricetag.jumbotron > a').click();
     cy.get('#product > fieldset > div:nth-child(6) > ul > li > a').click();
     cy.get('#categorymenu > nav > ul > li:nth-child(3) > a').click();
     cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div:nth-child(3) > div.thumbnail > div.pricetag.jumbotron > a > i').click();
-    cy.get('#product > fieldset > div:nth-child(5) > ul > li > a').click();
+    cy.get('#categorymenu > nav > ul > li:nth-child(6) > a').click();
     cy.get('#categorymenu > nav > ul > li:nth-child(4) > a').click();
     cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div:nth-child(2) > div.thumbnail > div.pricetag.jumbotron > a > i').click();
-    cy.get('#categorymenu > nav > ul > li.current > a').click()
+    cy.get('#categorymenu > nav > ul > li:nth-child(8) > a').click();
+    cy.get('#categorymenu > nav > ul > li:nth-child(5) > a').click();
+    cy.get('#maincontainer > div > div > div > div > div.thumbnails.grid.row.list-inline > div:nth-child(1) > div.thumbnail > div.pricetag.jumbotron > a > i').click()
     cy.get('body > div > header > div.container-fluid > div > div.block_7 > ul > li > a').click()
     cy.get('#cart > div > div.container-fluid.cart-info.product-list > table > tbody > tr:nth-child(3) > td:nth-child(7) > a').click()
     cy.get('#cart_quantity12180e3a92dbdbee7dcadd3b4b20cf25899').clear()
     cy.get('#cart_quantity12180e3a92dbdbee7dcadd3b4b20cf25899').type('2')
     
-
+    
+    cy.get('#cart_checkout1').click()
+    let address2
+    cy.get("#maincontainer > div > div.col-md-9.col-xs-12.mt20 > div > div.contentpanel > table.table.confirm_shippment_options > tbody > tr > td:nth-child(2) > address").invoke("text").then((text)=>{
+        address2=text.substring(text.indexOf(" ")+"".length, text.indexOf("Bucuresti"));
+        cy.log(address2)
+    if(address1==address2)
+        cy.log("Address is the default one")
+    else
+        cy.log("address is not the default one")
 })
-*/
+    cy.get('#checkout_btn').click()
+    cy.get('#maincontainer > div > div > div > div > section > p:nth-child(4) > a').click()
+    cy.get("#maincontainer > div > div > div > div > div:nth-child(1) > table > tbody > tr > td:nth-child(1)").invoke("text").then((text)=>{
+        orderId=text.substring(text.indexOf("Order ID")+"Order ID".length, text.indexOf("Status")).trim().split('#')[1];
+        cy.log(orderId)
+    cy.get('#maincontainer > div > div.column_right.col-md-3.col-xs-12.mt20 > div.sidewidt > div > ul > li:nth-child(6) > a').click()
+    
+    cy.get('#maincontainer > div > div.col-md-9.col-xs-12.mt20 > div > div > div:nth-child(1)').invoke("text").then((text)=>{
+        orderId2=text.substring(text.indexOf('#')+'#'.length, text.indexOf("Status"));
+        cy.log(orderId2)
+    if(Number(orderId)==Number(orderId2))
+        cy.log('Ok')
+    else
+        cy.log('not ok')
+    }) 
+    })
+    cy.get('#maincontainer > div > div.col-md-9.col-xs-12.mt20 > div > div > div:nth-child(1) > div:nth-child(2)').contains("Status: Pending")
+        
+
+    
+})
 })   
--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -32,7 +32,7 @@ describe('Register and log in to-do app', () => {
     cy.get('#AccountFrm_agree').click();
     cy.get('#AccountFrm > div.form-group > div > div > button').click();
     cy.get('#maincontainer').click();
-    //cy.log('Successful register');
+  
   })
 
   it('Login with valid account', () => {
@@ -40,7 +40,8 @@ describe('Register and log in to-do app', () => {
       cy.get('#loginFrm_loginname').type('contulmeunou');
       cy.get('#loginFrm_password').type('parola123');
       cy.get('#loginFrm > fieldset > button').click();
-      //cy.log('Successful login');
+      cy.get('#maincontainer > div > div.col-md-9.col-xs-12.mt20 > div > h1 > span.maintext').contains(' My Account')
+      
   })
 
 
@@ -66,7 +67,7 @@ it('Register with invalid email', () => {
   cy.get('#AccountFrm_agree').click();
   cy.get('#AccountFrm > div.form-group > div > div > button').click();
   cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('Email Address does not appear to be valid!');
-  //cy.log('Register failed');
+  
 })
 
 //not validation: Phone number is incorrect 
@@ -76,8 +77,8 @@ it('Register with invalid phone number', () => {
   cy.get('#AccountFrm_firstname').type('123');
   cy.get('#AccountFrm_lastname').type('User');
   cy.get('#AccountFrm_email').type('email.nooou@gmail.com');
-  cy.get('#AccountFrm_telephone').type('07568925');
-  cy.get('#AccountFrm_fax').type('07568925');
+  cy.get('#AccountFrm_telephone').type('0756');
+  cy.get('#AccountFrm_fax').type('0756');
   cy.get('#AccountFrm_company').type('My company srl');
   cy.get('#AccountFrm_address_1').type('Suceava');
   cy.get('#AccountFrm_address_2').type('Bucuresti');
@@ -90,9 +91,8 @@ it('Register with invalid phone number', () => {
   cy.get('#AccountFrm_confirm').type('parola123');
   cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
   cy.get('#AccountFrm_agree').click();
-  cy.get('#AccountFrm > div.form-group > div > div > button').click();
-  cy.get('#maincontainer').click();
-  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible');//expected
+  cy.get('#AccountFrm > div.form-group > div > div > button').click(); 
+  //cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible');//expected
  
 }) 
 
@@ -101,7 +101,7 @@ it('Register with invalid name', () => {
   cy.get('#customer_menu_top').click();
   cy.get('#accountFrm > fieldset > button').click();
   cy.get('#AccountFrm_lastname').type('User');
-  cy.get('#AccountFrm_email').type('email');
+  cy.get('#AccountFrm_email').type('email1@gmail.com');
   cy.get('#AccountFrm_telephone').type('0756892585');
   cy.get('#AccountFrm_fax').type('0756892585');
   cy.get('#AccountFrm_company').type('My company srl');
@@ -117,10 +117,7 @@ it('Register with invalid name', () => {
   cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
   cy.get('#AccountFrm_agree').click();
   cy.get('#AccountFrm > div.form-group > div > div > button').click();
-  //cy.get('#maincontainer').click();
   cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('First Name must be between 1 and 32 characters!');
-  cy.get('.help-block').contains('First Name must be between 1 and 32 characters!');
-  //cy.log('Register failed');
 }) 
 
 it('Register with invalid name >32', () => {
@@ -144,10 +141,9 @@ it('Register with invalid name >32', () => {
   cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
   cy.get('#AccountFrm_agree').click();
   cy.get('#AccountFrm > div.form-group > div > div > button').click();
-  //cy.get('#maincontainer').click();
   cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('First Name must be between 1 and 32 characters!');
   cy.get('.help-block').contains('First Name must be between 1 and 32 characters!');
-  //cy.log('Register failed');
+
 }) 
 
 
@@ -172,10 +168,9 @@ it('Register with invalid login name', () => {
   cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
   cy.get('#AccountFrm_agree').click();
   cy.get('#AccountFrm > div.form-group > div > div > button').click();
-  //cy.get('#maincontainer').click();
   cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('Login name must be alphanumeric only and between 5 and 64 characters!');
   cy.get('#AccountFrm > div:nth-child(9) > fieldset > div:nth-child(1) > span').contains('Login name must be alphanumeric only and between 5 and 64 characters!');
-  //cy.log('Register failed');
+
 }) 
 
 
@@ -203,10 +198,10 @@ it('Register with invalid password confirm', () => {
   cy.get('#AccountFrm > div.form-group > div > div > button').click();
  cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').contains('Password confirmation does not match password!');
  cy.get('#AccountFrm > div:nth-child(9) > fieldset > div.form-group.has-error > span').contains('Password confirmation does not match password!');
- //cy.log('Register failed');
+
 }) 
 
-it('Register with empty fields', () => {
+it.only('Register with empty fields', () => {
   cy.get('#customer_menu_top').click();
   cy.get('#accountFrm > fieldset > button').click();
   cy.get('#AccountFrm > div:nth-child(11) > fieldset > div > div > label:nth-child(1)').click();
@@ -221,7 +216,7 @@ it('Register with empty fields', () => {
   .contains('Zip/postal code must be between 3 and 10 characters!')
   .contains('Please select a region / state!')
   .contains('Password must be between 4 and 20 characters!');
-  //cy.log('Register failed');
+  
 }) 
 
 it('Login with invalid login name', () => {
@@ -230,23 +225,21 @@ it('Login with invalid login name', () => {
   cy.get('#loginFrm_password').type('parola123');
   cy.get('#loginFrm > fieldset > button').click();
   cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible').contains('Error: Incorrect login or password provided.');
-  //cy.log('Filed login');
 })
 
-it.skip('Login with invalid password', () => {
+it('Login with invalid password', () => {
   cy.get('#customer_menu_top').click();
   cy.get('#loginFrm_loginname').type('contulmeunou');
   cy.get('#loginFrm_password').type('parola123456');
   cy.get('#loginFrm > fieldset > button').click();
   cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible').contains('Error: Incorrect login or password provided.');
-  //cy.log('Filed login');
 })
 
 it('Login with empty fields', () => {
   cy.get('#customer_menu_top').click();
   cy.get('#loginFrm > fieldset > button').click();
   cy.get('#maincontainer > div > div > div > div.alert.alert-error.alert-danger').should('be.visible').contains('Error: Incorrect login or password provided.');
-  //cy.log('Filed login');
+ 
 })
 
 })
