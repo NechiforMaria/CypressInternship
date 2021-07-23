a6c80a34e063f80637e58fd99d44bceaf465b30e
NechiforMaria <bianca.nechifor@student.usv.ro>
2021-07-23 11:13:50 +0300

    Task 4

--- cypress/integration/task4.spec.js
+++ cypress/integration/task4.spec.js
@@ -6,13 +6,8 @@ describe('API Testing', () => {
         Object.entries(response.body).forEach(element=>{
             cy.log(element)
         })
-        Cypress.Screenshot.defaults(options)
+        
     })
     })
      
-    /*
-    it.only('Read2',()=>{
-        const item={"sold","string","pending":"6","available":"761","StatusJvR":"2"}
-        cy.request('POST','https://petstore.swagger.io/v2/store/inventory',  item)
-    })*/
  })   
